0f56a665d34752412fbfbc755e0f0b1e520ffba6
DropD <r.haeuselmann@gmx.ch>
2017-01-24 12:47:55 +0100

    fix parser entry points

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ if __name__ == '__main__':
                 'quantumespresso.pwimmigrant = aiida_quantumespresso.calculations:PwimmigrantCalculation',
             ],
             'aiida.parsers': [
-              'quantumespresso.basicpw = aiida_quantumespresso.basicpw:BasicpwParser',
-              'quantumespresso.cp = aiida_quantumespresso.plugins.cp:CpParser',
-              'quantumespresso.pw = aiida_quantumespresso.plugins.pw:PwParser',
+              'quantumespresso.basicpw = aiida_quantumespresso.parsers.basicpw:BasicpwParser',
+              'quantumespresso.cp = aiida_quantumespresso.parsers.cp:CpParser',
+              'quantumespresso.pw = aiida_quantumespresso.parsers.pw:PwParser',
             ],
             'aiida.tools.dbexporters.tcod_plugins': [
               'cp = aiida_quantumespresso.dbexport.tcodplugins.cp:CpTcodtranslator',
