df938c5d6b8fb14f423d7a2308d304f357f8c119
DropD <r.haeuselmann@gmx.ch>
2017-02-13 19:09:11 +0100

    put most setup information into parseable json.

--- setup.py
+++ setup.py
@@ -1,37 +1,12 @@
 from setuptools import setup, find_packages
+import json
+
 
 if __name__ == '__main__':
+    with open('setup.json', 'r') as info:
+        kwargs = json.load(info)
     setup(
-        version='0.7.1b0',
-        name='aiida_quantumespresso',
-        url='http://www.aiida.net',
-        license='MIT License',
-        author='The AiiDA team',
-        author_email='developers@aiida.net',
         include_package_data=True,
-        classifiers=[
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 2.7',
-            'Development Status :: 4 - Beta',
-        ],
-        install_requires=[
-            'aiida[ssh]'
-        ],
         packages=find_packages(),
-        entry_points={
-            'aiida.calculations': [
-                'quantumespresso.pw = aiida_quantumespresso.calculations.pw:PwCalculation',
-                'quantumespresso.cp = aiida_quantumespresso.calculations.cp:CpCalculation',
-                'quantumespresso.pwimmigrant = aiida_quantumespresso.calculations:PwimmigrantCalculation',
-            ],
-            'aiida.parsers': [
-              'quantumespresso.basicpw = aiida_quantumespresso.parsers.basicpw:BasicpwParser',
-              'quantumespresso.cp = aiida_quantumespresso.parsers.cp:CpParser',
-              'quantumespresso.pw = aiida_quantumespresso.parsers.pw:PwParser',
-            ],
-            'aiida.tools.dbexporters.tcod_plugins': [
-              'cp = aiida_quantumespresso.dbexport.tcodplugins.cp:CpTcodtranslator',
-              'pw = aiida_quantumespresso.dbexport.tcodplugins.pw:PwTcodtranslator'
-            ]
-        }
+        **kwargs
     )
