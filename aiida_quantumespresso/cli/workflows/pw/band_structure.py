7791093f232933797e21a034003b4003165161eb
Sebastiaan Huber <mail@sphuber.net>
2018-03-23 10:38:18 +0100

    Add the `daemon` option to all cli launch scripts

--- aiida_quantumespresso/cli/workflows/pw/band_structure.py
+++ aiida_quantumespresso/cli/workflows/pw/band_structure.py
@@ -8,19 +8,20 @@ from aiida_quantumespresso.utils.click import options
 @options.code()
 @options.structure()
 @options.pseudo_family()
+@options.daemon()
 @click.option(
     '-z', '--protocol', type=click.Choice(['standard']), default='standard', show_default=True,
     help='the protocol to use for the workflow'
 )
 def launch(
-    code, structure, pseudo_family, protocol):
+    code, structure, pseudo_family, daemon, protocol):
     """
     Run the PwBandStructureWorkChain for a given input structure 
     to compute the band structure for the relaxed structure
     """
     from aiida.orm.data.base import Str
     from aiida.orm.utils import WorkflowFactory
-    from aiida.work.run import run
+    from aiida.work.run import run, submit
 
     PwBandStructureWorkChain = WorkflowFactory('quantumespresso.pw.band_structure')
 
@@ -31,4 +32,8 @@ def launch(
         'protocol': Str(protocol),
     }
 
-    run(PwBandStructureWorkChain, **inputs)
+    if daemon:
+        workchain = submit(PwBandStructureWorkChain, **inputs)
+        click.echo('Submitted {}<{}> to the daemon'.format(PwBandStructureWorkChain.__name__, workchain.pid))
+    else:
+        run(PwBandStructureWorkChain, **inputs)
