e927e17a17a0b4758f8da8fa13287a2b66f3eb11
Sebastiaan Huber <mail@sphuber.net>
2018-06-06 14:46:38 +0200

    Fix update aiida core requirement to v1.0.0a1 (#151)
    Up requirement of aiida-core and set lower bound to v1.0.0a1
    
    Also adapted the Travis scripts to aiida-core v1.0.0, which included
    adding RabbitMQ to the `.travis.yml` and changing the way the default
    profile is set, which changed in v1.0.0

--- .travis-data/setup_profiles.sh
+++ .travis-data/setup_profiles.sh
@@ -5,6 +5,7 @@ if [[ "$TEST_TYPE" != "pre-commit" ]]
 then
     # Here I create the actual DB for submission
     psql -c "CREATE DATABASE $TEST_AIIDA_BACKEND;" -U postgres
+
     # Here I create the test DB
     psql -c "CREATE DATABASE test_$TEST_AIIDA_BACKEND;" -U postgres
 
@@ -14,7 +15,6 @@ then
     # Here I setup the test AiiDA profile, non-interactively
     verdi -p test_$TEST_AIIDA_BACKEND setup --non-interactive --backend=$TEST_AIIDA_BACKEND --email="aiida@localhost" --db_host="localhost" --db_port=5432 --db_name="test_$TEST_AIIDA_BACKEND" --db_user=postgres --db_pass='' --repo="/tmp/test_repository_test_${TEST_AIIDA_BACKEND}/" --first-name=AiiDA --last-name=test --institution="AiiDA Team" --no-password
 
-    # Maybe not needed, but set this profile to be the default one for (at least) the daemon
-    verdi profile setdefault daemon $TEST_AIIDA_BACKEND
-    verdi profile setdefault verdi $TEST_AIIDA_BACKEND
+    # Maybe not needed, but set this profile to be the default one
+    verdi profile setdefault $TEST_AIIDA_BACKEND
 fi
