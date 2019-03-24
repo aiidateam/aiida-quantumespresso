1e531dd43bf4e5229364a3a78daf3ec4088564e2
Sebastiaan Huber <mail@sphuber.net>
2019-03-24 19:04:39 +0100

    Remove `.ci` and `setup.py` from pre-commit exclusion list

--- .ci/precommit/fix_setup_version.py
+++ .ci/precommit/fix_setup_version.py
@@ -1,21 +1,23 @@
+"""Pre-commit script to ensure that version numbers in `setup.json` and `aiida_quantumespresso/__init__.py` match."""
+# pylint: disable=wrong-import-position
 import os
 import json
 import sys
 
-this_path = os.path.split(os.path.realpath(__file__))[0]
+FILEPATH = os.path.split(os.path.realpath(__file__))[0]
 
 # Get current JSON content
-setup_path = os.path.join(this_path, os.pardir, os.pardir, 'setup.json')
-with open(setup_path) as f:
-	setup_content = json.load(f)
+FILEPATH_SETUP_JSON = os.path.join(FILEPATH, os.pardir, os.pardir, 'setup.json')
+with open(FILEPATH_SETUP_JSON) as f:
+    CONTENT_SETUP_JSON = json.load(f)
 
 # Retrieve version from python package
-sys.path.insert(0, os.path.join(this_path, os.pardir, os.pardir))
+sys.path.insert(0, os.path.join(FILEPATH, os.pardir, os.pardir))
 import aiida_quantumespresso
-version = aiida_quantumespresso.__version__
+VERSION = aiida_quantumespresso.__version__
 
-setup_content['version'] = version
+CONTENT_SETUP_JSON['version'] = VERSION
 
 # Rewrite JSON in a 'consistent' way (sorted, indented)
-with open(setup_path, 'w') as f:
-	json.dump(setup_content, f, indent=4, sort_keys=True)
+with open(FILEPATH_SETUP_JSON, 'w') as f:
+    json.dump(CONTENT_SETUP_JSON, f, indent=4, sort_keys=True)
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+"""Setup script for aiida-core package."""
 import json
+from setuptools import setup, find_packages
 
 if __name__ == '__main__':
     with open('setup.json', 'r') as info:
-        kwargs = json.load(info)
-    setup(include_package_data=True, reentry_register=True, packages=find_packages(), **kwargs)
+        SETUP_JSON = json.load(info)
+    setup(include_package_data=True, reentry_register=True, packages=find_packages(), **SETUP_JSON)
