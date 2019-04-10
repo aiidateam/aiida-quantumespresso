5862ee8473541f270f1141660751d07b18077e10
Sebastiaan Huber <mail@sphuber.net>
2019-04-10 09:02:43 +0200

    Remove hard coding of available XML parser schemas
    Instead, they are now auto-discovered by simply inspecting the schemas
    directory for all available `.xsd` files. This way one only needs to add
    a new XSD schema file to the directory for it to become available to the
    parser.

--- aiida_quantumespresso/parsers/parse_xml/pw/versions.py
+++ aiida_quantumespresso/parsers/parse_xml/pw/versions.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+
 import enum
 import os
+
 from defusedxml import ElementTree
 
 DEFAULT_SCHEMA_FILENAME = 'qes-1.0.xsd'
+DIRNAME_SCHEMAS = 'schemas'
+DIRPATH_SCHEMAS = os.path.join(os.path.dirname(os.path.abspath(__file__)), DIRNAME_SCHEMAS)
 
 
 class QeXmlVersion(enum.Enum):
@@ -15,8 +19,7 @@ class QeXmlVersion(enum.Enum):
 
 
 def get_xml_file_version(xml_file):
-    """
-    Get the version of the Quantum ESPRESSO pw.x XML output file
+    """Return the version of the Quantum ESPRESSO pw.x XML output file
 
     :param xml_file: absolute path to the XML file
     :raises ValueError: if the file cannot be read, parsed or if the version cannot be determined
@@ -35,7 +38,10 @@ def get_xml_file_version(xml_file):
 
 
 def get_schema_filepath(xml):
-    """
+    """Return the absolute filepath to the XML schema file that can be used to parse the given XML
+
+    :param xml: the pre-parsed XML object
+    :return: the XSD absolute filepath
     """
     schema_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'schemas')
     schema_filename = get_schema_filename(xml)
@@ -45,7 +51,10 @@ def get_schema_filepath(xml):
 
 
 def get_default_schema_filepath():
-    """
+    """Return the absolute filepath to the default XML schema file
+
+    :param xml: the pre-parsed XML object
+    :return: the XSD absolute filepath
     """
     schema_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'schemas')
     schema_filename = DEFAULT_SCHEMA_FILENAME
@@ -55,11 +64,12 @@ def get_default_schema_filepath():
 
 
 def get_schema_filename(xml):
+    """Return the filename of the schema file that corresponds to the given XML object
+
+    :param xml: the pre-parsed XML object
+    :return: the XSD filename
     """
-    Returns the name of the schema file that corresponds to the given XML object
-    """
-    
-    XML_SCHEMA_LOCATION_KEY = '{http://www.w3.org/2001/XMLSchema-instance}schemaLocation'
+    xml_schema_location_key = '{http://www.w3.org/2001/XMLSchema-instance}schemaLocation'
     # The part in curly brackets is an expanded namespace
 
     element_root = xml.getroot()
@@ -67,7 +77,7 @@ def get_schema_filename(xml):
     if element_root is None:
         return None
 
-    element_schema_location = element_root.get(XML_SCHEMA_LOCATION_KEY)
+    element_schema_location = element_root.get(xml_schema_location_key)
     # e.g. "http://www.quantum-espresso.org/ns/qes/qes-1.0 http://www.quantum-espresso.org/ns/qes/qes-1.0.xsd"
 
     if element_schema_location is None:
@@ -75,31 +85,36 @@ def get_schema_filename(xml):
 
     schema_location = element_schema_location.split()[1]  # e.g. "http://www.quantum-espresso.org/ns/qes/qes-1.0.xsd"
     schema_filename = schema_location.rpartition('/')[2]  # e.g. "qes-1.0.xsd"
-    
+
     return schema_filename
 
 
+def get_available_xml_schemas():
+    """Return the filenames of the available XML schemas.
+
+    These are essentially all the files with the .xsd extenstion in the `DIRPATH_SCHEMAS` folder
+
+    :return: a list of XML schema filenames
+    """
+    return [file for file in os.listdir(DIRPATH_SCHEMAS) if file.endswith('.xsd')]
+
+
 def is_valid_post_6_2_version(xml):
+    """Return whether the given XML object corresponds to an XML output file of Quantum ESPRESSO pw.x post v6.2
+
+    These versions of the output XML are parsable with an XSD schema file
+
+    :param xml: a parsed XML output file
+    :return: boolean, True when the XML is parsable with an XSD schema file, False otherwise
     """
-    Returns whether the given XML object corresponds to an XML output file of Quantum ESPRESSO pw.x post v6.2
-
-    This version of the output XML is parsable with an XSD schema file
-    """
-    KNOWN_SCHEMA_VERSIONS = [
-        'qes-1.0.xsd',
-        'qes_181201.xsd',
-        'qes_190207.xsd',
-        'qes_190304.xsd',  # changes n_opt_steps from positiveInteger to integer
-    ]
-
-    schema_filename = get_schema_filename(xml)
-
-    return schema_filename in KNOWN_SCHEMA_VERSIONS
+    return get_schema_filename(xml) in get_available_xml_schemas()
 
 
 def is_valid_pre_6_2_version(xml):
-    """
-    Returns whether the given XML object corresponds to an XML output file of Quantum ESPRESSO pw.x pre v6.2
+    """Returns whether the given XML object corresponds to an XML output file of Quantum ESPRESSO pw.x pre v6.2
+
+    :param xml: a parsed XML output file
+    :return: boolean, True when the XML was produced by Quantum ESPRESSO with the old XML format
     """
     element_header = xml.find('HEADER')
 
