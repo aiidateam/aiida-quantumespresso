3c63b93700679a6e9206ebf6747fb40f18683495
Oscar David Arbeláez <oscar.arbelaez@epfl.ch>
2019-05-08 10:46:06 +0200

    Move test specific configuration out of conftest file

--- tests/parsers/test_cp.py
+++ tests/parsers/test_cp.py
@@ -3,9 +3,63 @@
 """Tests for the `PwParser`."""
 from __future__ import absolute_import
 
+from aiida import orm
+from aiida.common import AttributeDict
+from ase.spacegroup import crystal
+import pytest
+
+
+@pytest.fixture
+def cp_inputs():
+    """
+    Minimal input for cp calculations.
+    """
+    alat = 5.4
+    ase_structure = crystal(
+        "Si",
+        [(0, 0, 0)],
+        spacegroup=227,
+        cellpar=[alat, alat, alat, 90, 90, 90],
+        primitive_cell=True,
+    )
+    structure = orm.StructureData(ase=ase_structure)
+    structure.store()
+    parameters = {
+        'CONTROL': {
+            'calculation': "cp",
+            'restart_mode': "from_scratch",
+            'wf_collect': False,
+            'iprint': 1,
+            'isave': 100,
+            'dt': 3.0,
+            'max_seconds': 25 * 60,
+            'nstep': 10,
+        },
+        'SYSTEM': {
+            'ecutwfc': 30.0,
+            'ecutrho': 240.0,
+            'nr1b': 24,
+            'nr2b': 24,
+            'nr3b': 24,
+        },
+        'ELECTRONS': {
+            'electron_damping': 1.0e-1,
+            'electron_dynamics': "damp",
+            'emass': 400.0,
+            'emass_cutoff': 3.0,
+        },
+        'IONS': {
+            'ion_dynamics': "none"
+        },
+    }
+    return AttributeDict({
+        'structure': structure,
+        'parameters': orm.Dict(dict=parameters),
+        'settings': orm.Dict()
+    })
 
 def test_cp_default(fixture_database, fixture_computer_localhost, generate_calc_job_node, generate_parser,
-                    generate_inputs, data_regression):
+                    cp_inputs, data_regression):
     """Test a default `cp.x` calculation.
 
     The output is created by renning a dead simple SCF calculation for a silicon structure.
@@ -14,9 +68,7 @@ def test_cp_default(fixture_database, fixture_computer_localhost, generate_calc_
     entry_point_calc_job = 'quantumespresso.cp'
     entry_point_parser = 'quantumespresso.cp'
 
-    input_nodes = generate_inputs(entry_point_calc_job)
-
-    node = generate_calc_job_node(entry_point_calc_job, fixture_computer_localhost, 'default', input_nodes)
+    node = generate_calc_job_node(entry_point_calc_job, fixture_computer_localhost, 'default', cp_inputs)
     parser = generate_parser(entry_point_parser)
     results, calcfunction = parser.parse_from_node(node, store_provenance=False)
 
--- tests/parsers/test_pw.py
+++ tests/parsers/test_pw.py
@@ -3,9 +3,37 @@
 """Tests for the `PwParser`."""
 from __future__ import absolute_import
 
+import pytest
+from aiida import orm
+from aiida.common import AttributeDict
+
+
+@pytest.fixture
+def pw_inputs():
+    structure = orm.StructureData()
+    parameters = {
+        'CONTROL': {
+            'calculation': 'scf'
+        },
+        'SYSTEM': {
+            'ecutrho': 240.0,
+            'ecutwfc': 30.0
+        }
+    }
+    kpoints = orm.KpointsData()
+    kpoints.set_cell_from_structure(structure)
+    kpoints.set_kpoints_mesh_from_density(0.15)
+
+    return AttributeDict({
+        'structure': structure,
+        'kpoints': kpoints,
+        'parameters': orm.Dict(dict=parameters),
+        'settings': orm.Dict()
+    })
+
 
 def test_pw_default(fixture_database, fixture_computer_localhost, generate_calc_job_node, generate_parser, 
-                    generate_inputs, data_regression):
+                    pw_inputs, data_regression):
     """Test a default `pw.x` calculation.
 
     The output is created by running a dead simple SCF calculation for a silicon structure.
@@ -14,9 +42,7 @@ def test_pw_default(fixture_database, fixture_computer_localhost, generate_calc_
     entry_point_calc_job = 'quantumespresso.pw'
     entry_point_parser = 'quantumespresso.pw'
 
-    input_nodes = generate_inputs(entry_point_calc_job)
-
-    node = generate_calc_job_node(entry_point_calc_job, fixture_computer_localhost, 'default', input_nodes)
+    node = generate_calc_job_node(entry_point_calc_job, fixture_computer_localhost, 'default', pw_inputs)
     parser = generate_parser(entry_point_parser)
     results, calcfunction = parser.parse_from_node(node, store_provenance=False)
 
