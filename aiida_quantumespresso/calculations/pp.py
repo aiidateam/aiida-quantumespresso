3c3b0617409247b1c44dff6ac61d586598f94885
Sebastiaan Huber <mail@sphuber.net>
2019-05-10 13:14:46 +0200

    Finalize migration of `PpCalculation` and add launcher

--- aiida_quantumespresso/calculations/pp.py
+++ aiida_quantumespresso/calculations/pp.py
@@ -1,37 +1,25 @@
 # -*- coding: utf-8 -*-
-
 from __future__ import absolute_import
+
 from aiida.orm import RemoteData, FolderData
 from aiida_quantumespresso.calculations.namelists import NamelistsCalculation
 
 
 class PpCalculation(NamelistsCalculation):
-    """
-    `pp.x` code of the Quantum ESPRESSO distribution, handles the
-    post-processing of charge-densities, potentials, ...
-    For more information, refer to http://www.quantum-espresso.org/
-    """
+    """`CalcJob` implementation for the pp.x code of Quantum ESPRESSO."""
 
-    _FILPLOT = "aiida.filplot"
+    _FILPLOT = 'aiida.filplot'
 
-    _default_namelists = ["INPUTPP", "PLOT"]
-
-    _blocked_keywords = [
-        ("INPUTPP", "outdir", NamelistsCalculation._OUTPUT_SUBFOLDER),
-        ("INPUTPP", "prefix", NamelistsCalculation._PREFIX),
-        ("INPUTPP", "filplot", NamelistsCalculation._FILPLOT),
-    ]
-
-    _default_parser = None
-
+    _default_namelists = ['INPUTPP', 'PLOT']
     _internal_retrieve_list = [_FILPLOT]
+    _blocked_keywords = [
+        ('INPUTPP', 'outdir', NamelistsCalculation._OUTPUT_SUBFOLDER),
+        ('INPUTPP', 'prefix', NamelistsCalculation._PREFIX),
+        ('INPUTPP', 'filplot', _FILPLOT),
+    ]
 
     @classmethod
     def define(cls, spec):
         super(PpCalculation, cls).define(spec)
-        spec.input(
-            "parent_folder",
-            valid_type=(RemoteData, FolderData),
-            required=True,
-            help="Output folder of a PW calculation",
-        )
+        spec.input('parent_folder', valid_type=(RemoteData, FolderData),
+            help='Output folder of a completed `PwCalculation`')
--- aiida_quantumespresso/cli/calculations/__init__.py
+++ aiida_quantumespresso/cli/calculations/__init__.py
@@ -5,6 +5,7 @@ from .cp import launch_calculation
 from .dos import launch_calculation
 from .matdyn import launch_calculation
 from .ph import launch_calculation
+from .pp import launch_calculation
 from .pw import launch_calculation
 from .pw2wannier90 import launch_calculation
 from .projwfc import launch_calculation
