b48cc904ab0a9a8c3208e56d660967c7eb9f158b
Sebastiaan Huber <mail@sphuber.net>
2019-05-31 10:27:03 +0200

    Add a test for the `PwParser` for a `relax` calculation
    Note that since the raw output parser depends explicitly on the input
    parameters, for example to determine what type of calculation was run,
    the inputs need to contain the correct calculation type or the parser
    will not parse all the output nodes. That is why the `relax` test
    requires a separate input node generator function, in order to be able
    to set the correct `calculation` type in the input parameters.

--- tests/parsers/test_pw.py
+++ tests/parsers/test_pw.py
@@ -9,7 +9,7 @@ from aiida.common import AttributeDict
 
 
 @pytest.fixture
-def generate_inputs():
+def generate_inputs_default():
     """Return only those inputs that the parser will expect to be there."""
     structure = orm.StructureData()
     parameters = {
@@ -33,9 +33,46 @@ def generate_inputs():
     })
 
 
+@pytest.fixture
+def generate_inputs_relax():
+    """Return only those inputs that the parser will expect to be there.
+
+    This needs a separate input generation function from the default one, because the parser depends on certain values
+    in the input parameters to determine what kind of calculation it was. For example, it will check the card
+    `CONTROL.calculation` to determine whether the `TrajectoryData` should be attached. If we would not set it to
+    `relax`, the parser would not parse that output node and the test would fail. Until we can make the raw output
+    parser independent of the input parameters, this will have to remain a separate test inputs generator.
+    """
+    a = 5.43
+    structure = orm.StructureData(cell=[[a / 2., a / 2., 0], [a / 2., 0, a / 2.], [0, a / 2., a / 2.]])
+    structure.append_atom(position=(0., 0., 0.), symbols='Si')
+    structure.append_atom(position=(a / 4., a / 4., a / 4.), symbols='Si')
+    structure.store()
+
+    parameters = {
+        'CONTROL': {
+            'calculation': 'relax'
+        },
+        'SYSTEM': {
+            'ecutrho': 240.0,
+            'ecutwfc': 30.0
+        }
+    }
+    kpoints = orm.KpointsData()
+    kpoints.set_cell_from_structure(structure)
+    kpoints.set_kpoints_mesh_from_density(0.15)
+
+    return AttributeDict({
+        'structure': structure,
+        'kpoints': kpoints,
+        'parameters': orm.Dict(dict=parameters),
+        'settings': orm.Dict()
+    })
+
+
 def test_pw_default(fixture_database, fixture_computer_localhost, generate_calc_job_node, generate_parser,
-                    generate_inputs, data_regression):
-    """Test a default `pw.x` calculation.
+                    generate_inputs_default, data_regression):
+    """Test a `pw.x` calculation in `scf` mode.
 
     The output is created by running a dead simple SCF calculation for a silicon structure.
     This test should test the standard parsing of the stdout content and XML file stored in the standard results node.
@@ -43,7 +80,7 @@ def test_pw_default(fixture_database, fixture_computer_localhost, generate_calc_
     entry_point_calc_job = 'quantumespresso.pw'
     entry_point_parser = 'quantumespresso.pw'
 
-    node = generate_calc_job_node(entry_point_calc_job, fixture_computer_localhost, 'default', generate_inputs)
+    node = generate_calc_job_node(entry_point_calc_job, fixture_computer_localhost, 'default', generate_inputs_default)
     parser = generate_parser(entry_point_parser)
     results, calcfunction = parser.parse_from_node(node, store_provenance=False)
 
@@ -51,3 +88,32 @@ def test_pw_default(fixture_database, fixture_computer_localhost, generate_calc_
     assert calcfunction.is_finished_ok, calcfunction.exit_message
     assert 'output_parameters' in results
     data_regression.check(results['output_parameters'].get_dict())
+
+
+def test_pw_relax(fixture_database, fixture_computer_localhost, generate_calc_job_node, generate_parser,
+                  generate_inputs_relax, data_regression):
+    """Test a `pw.x` calculation in `relax` mode.
+
+    The output is created by running a dead 'relax' calculation for a silicon structure.
+    Note that `output_band` will not be there, because the bands are only parsed if the `retrieved_temporary_folder` is
+    passed containing the necessary files. Since we don't pass those in this test, the output node will not be created.
+    """
+    entry_point_calc_job = 'quantumespresso.pw'
+    entry_point_parser = 'quantumespresso.pw'
+
+    node = generate_calc_job_node(entry_point_calc_job, fixture_computer_localhost, 'relax', generate_inputs_relax)
+    parser = generate_parser(entry_point_parser)
+    results, calcfunction = parser.parse_from_node(node, store_provenance=False)
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_finished_ok, calcfunction.exit_message
+    assert 'output_kpoints' in results
+    assert 'output_parameters' in results
+    assert 'output_structure' in results
+    assert 'output_trajectory' in results
+    data_regression.check({
+        'output_kpoints': results['output_kpoints'].attributes,
+        'output_parameters': results['output_parameters'].get_dict(),
+        'output_structure': results['output_structure'].attributes,
+        'output_trajectory': results['output_trajectory'].attributes,
+    })
