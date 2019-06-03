65844dc3a6f55066ce1794619ea129971de8a92a
Sebastiaan Huber <mail@sphuber.net>
2019-06-03 11:50:12 +0200

    `PwParser`: refactor the handling of error messages
    Currently, both the standard output and the XML output are parsed. The
    current approach is based on older versions of Quantum ESPRESSO where
    the XML output was a sub set of that of the standard output. Therefore
    the XML was parsed first and then passed to the function that parses the
    stdout, using the XML output to complement where possible. With the
    recent addition of a schema-based XML output, were slowly moving towards
    making the XML output leading.
    
    It becomes very important to make the parsing of these two output
    sources as independent as possible. The `PwParser` can then decide how
    to merge the parsed data from both sources and which should take
    precedence. As well it should decide when and which error codes to
    return. Ideally, before exiting, it should parse as much information as
    possible.
    
    This commit makes a start with refactoring the parser and the raw
    parsers to separate these tasks as well as possible.

--- aiida_quantumespresso/utils/mapping.py
+++ aiida_quantumespresso/utils/mapping.py
@@ -7,6 +7,21 @@ from aiida.common import AttributeDict
 from aiida.orm import Dict
 
 
+def get_logging_container():
+    """Return an `AttributeDict` that can be used to map logging messages to certain log levels.
+
+    This datastructure is useful to add log messages in a function that does not have access to the right logger. Once
+    returned, the caller who does have access to the logger can then easily loop over the contents and pipe the messages
+    through the actual logger.
+
+    :return: :py:class:`~aiida.common.extendeddicts.AttributeDict`
+    """
+    return AttributeDict({
+        'warning': [],
+        'error': [],
+    })
+
+
 def update_mapping(original, source):
     """Update a nested dictionary with another optionally nested dictionary.
 
