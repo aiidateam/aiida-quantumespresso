9b039d3011191b611ecce0d67b747d7dfefcb4d4
Sebastiaan Huber <mail@sphuber.net>
2020-03-04 09:29:34 +0100

    `generate_calc_job_node`: use structure from input file if present
    Certain parsers will expect the output with respect to the crystal
    structure to be commensurate with the input structure. So far the
    `generate_calc_job_node` was using a default silicon structure as input.
    This was fine for the initial error cases for which the reference output
    files were easy to generate. As these become more complicated one does
    not want to be forced to reproduce it with silicon if one already has an
    example reference file. Instead it would be useful to have the fixture
    just get the input structure by parsing it from the input file. The
    `generate_calc_job_node` is updated to do just this: it will try to
    parse the structure from the `aiida.in` file in the fixture folder and
    if successful override the `structure` in the inputs.

--- tests/conftest.py
+++ tests/conftest.py
@@ -94,6 +94,14 @@ def generate_calc_job_node():
         from aiida.common import LinkType
         from aiida.plugins.entry_point import format_entry_point_string
 
+        filepath_folder = None
+
+        if test_name is not None:
+            basepath = os.path.dirname(os.path.abspath(__file__))
+            filename = os.path.join(entry_point_name[len('quantumespresso.'):], test_name)
+            filepath_folder = os.path.join(basepath, 'parsers', 'fixtures', filename)
+            filepath_input = os.path.join(filepath_folder, 'aiida.in')
+
         entry_point = format_entry_point_string('aiida.calculations', entry_point_name)
 
         node = orm.CalcJobNode(computer=computer, process_type=entry_point)
@@ -106,6 +114,14 @@ def generate_calc_job_node():
         if attributes:
             node.set_attribute_many(attributes)
 
+        if filepath_folder:
+            from qe_tools.utils.exceptions import ParsingError
+            from aiida_quantumespresso.tools.pwinputparser import PwInputFile
+            try:
+                inputs['structure'] = PwInputFile(filepath_input).get_structuredata()
+            except ParsingError:
+                pass
+
         if inputs:
             for link_label, input_node in flatten_inputs(inputs):
                 input_node.store()
@@ -113,14 +129,9 @@ def generate_calc_job_node():
 
         node.store()
 
-        if test_name is not None:
-            basepath = os.path.dirname(os.path.abspath(__file__))
-            filepath = os.path.join(
-                basepath, 'parsers', 'fixtures', entry_point_name[len('quantumespresso.'):], test_name
-            )
-
+        if filepath_folder:
             retrieved = orm.FolderData()
-            retrieved.put_object_from_tree(filepath)
+            retrieved.put_object_from_tree(filepath_folder)
             retrieved.add_incoming(node, link_type=LinkType.CREATE, link_label='retrieved')
             retrieved.store()
 
