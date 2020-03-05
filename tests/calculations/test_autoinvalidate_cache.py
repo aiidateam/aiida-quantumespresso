3bb6a032b1336b587ee2590d5a4b78e7f6150a54
Dominik Gresch <a-dogres@microsoft.com>
2020-03-05 16:38:25 +0100

    Add a test for the automatic 'invalidates_cache' attribute

---
+++ tests/calculations/test_autoinvalidate_cache.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Test the automatic 'invalidates_cache' attribute for exit codes."""
+
+from __future__ import absolute_import
+import pytest
+
+from aiida.plugins import CalculationFactory
+from aiida.plugins.entry_point import get_entry_point_names
+
+QE_CALC_ENTRY_POINT_NAMES = [
+    ep_name for ep_name in get_entry_point_names(group='aiida.calculations') if ep_name.startswith('quantumespresso')
+]
+
+# When explicitly overriden 'invalidates_cache' are added, add entries
+# EXPLICIT_OVERRIDES = {<entry_point_name>: {<status_integer>: <expected_value>}}
+
+EXPLICIT_OVERRIDES = {}
+
+
+@pytest.mark.parametrize('entry_point_name', QE_CALC_ENTRY_POINT_NAMES)
+def test_exit_code_invalidates_cache(entry_point_name):
+    """Test automatic 'invalidates_cache' attribute of exit codes.
+
+    Test that the 'invalidates_cache' attribute of exit codes is automatically
+    set according to the status integer.
+    """
+    calc_class = CalculationFactory(entry_point_name)
+    overrides = EXPLICIT_OVERRIDES.get(entry_point_name, {})
+    for exit_code in calc_class.exit_codes.values():
+        if exit_code.status in overrides:
+            assert exit_code.invalidates_cache == overrides[exit_code.status]
+        elif exit_code.status < 400:
+            assert exit_code.invalidates_cache
+        else:
+            assert not exit_code.invalidates_cache
