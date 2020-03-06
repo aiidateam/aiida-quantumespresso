413cfaab1ff4b476b36d625fcfe00ba02f17278a
Sebastiaan Huber <mail@sphuber.net>
2020-03-06 12:26:40 +0100

    `PwParser`: include external pressure in ionic convergence threshold (#470)
    The parser was not properly including an external pressure when checking
    the stress for convergence, leading the calculation to be unjustly as
    not converged. The stress is converged if the absolute difference of its
    corresponding pressure and the reference pressure is within the given
    threshold.

--- aiida_quantumespresso/utils/validation/trajectory.py
+++ aiida_quantumespresso/utils/validation/trajectory.py
@@ -6,7 +6,9 @@ from __future__ import division
 import numpy
 
 
-def verify_convergence_trajectory(trajectory, index=-1, threshold_forces=None, threshold_stress=None):
+def verify_convergence_trajectory(
+    trajectory, index=-1, threshold_forces=None, threshold_stress=None, reference_pressure=0.
+):
     """Verify that the data of the given `TrajectoryData` is converged with respect to the given thresholds.
 
     There are up to three properties controlled for convergence: total energy, forces and stress.
@@ -14,10 +16,14 @@ def verify_convergence_trajectory(trajectory, index=-1, threshold_forces=None, t
     array, or index within that array, does not exist in the `TrajectoryData`, None is returned. If all thresholds are
     successfully met, `True` is returned, if at least one of them fails, `False` is returned.
 
+    The stress is converged if the absolute difference of its corresponding pressure and the reference pressure is
+    within the given threshold.
+
     :param trajectory: the `TrajectoryData`
     :param index: the frame index of the trajectory data to check, default is `-1` meaning the last frame
     :param threshold_forces: the force threshold in Ry / bohr
     :param threshold_stress: the stress threshold in kbar
+    :param reference_pressure: reference pressure in kbar
     :return: `True` if all thresholds are valid, `False` otherwise
     :raises ValueError: if any of the arrays or indices don't exist
     """
@@ -27,7 +33,7 @@ def verify_convergence_trajectory(trajectory, index=-1, threshold_forces=None, t
         converged_forces = True
 
     if threshold_stress is not None:
-        converged_stress = verify_convergence_stress(trajectory, index, threshold_stress)
+        converged_stress = verify_convergence_stress(trajectory, index, threshold_stress, reference_pressure)
     else:
         converged_stress = True
 
@@ -58,12 +64,16 @@ def verify_convergence_forces(trajectory, index=-1, threshold=None):
     return numpy.max(abs(forces)) < threshold
 
 
-def verify_convergence_stress(trajectory, index=-1, threshold=None):
+def verify_convergence_stress(trajectory, index=-1, threshold=None, reference_pressure=0.):
     """Verify that the `stress` of the given `TrajectoryData` are converged with respect to given threshold.
+
+    The stress is converged if the absolute difference of its corresponding pressure and the reference pressure is
+    within the given threshold.
 
     :param trajectory: the `TrajectoryData`
     :param index: the frame index of the trajectory data to check, default is `-1` meaning the last frame
     :param threshold: the stress threshold in kbar
+    :param reference_pressure: reference pressure in kbar
     :return: `True` if threshold is valid, `False` otherwise
     :raises ValueError: if the `stress` array or given index does not exist
     """
@@ -71,10 +81,13 @@ def verify_convergence_stress(trajectory, index=-1, threshold=None):
         return None
 
     threshold /= 10.  # Convert to GPa
+    reference_pressure /= 10.  # Convert to GPa
 
     try:
         stress = trajectory.get_array('stress')[index]
     except (KeyError, IndexError):
         raise ValueError('the `stress` array does not exist or the given index exceeds the length.')
 
-    return (numpy.trace(stress) / 3.) < threshold
+    pressure = (numpy.trace(stress) / 3.)
+
+    return abs(pressure - reference_pressure) < threshold
