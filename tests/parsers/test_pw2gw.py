2ff5ea2c08a9d6dc06dace70b0f9e8c9121c8936
Crivella <davidecrivella@yahoo.it>
2020-03-25 20:01:31 +0100

    Adapted tests for new error codes and output nodes

--- tests/parsers/test_pw2gw.py
+++ tests/parsers/test_pw2gw.py
@@ -7,10 +7,7 @@ from aiida import orm
 
 
 def test_pw2gw_default(
-    aiida_profile,
-    fixture_localhost,
-    generate_parser,
-    generate_calc_job_node,
+    aiida_profile, fixture_localhost, generate_parser, generate_calc_job_node, data_regression, num_regression
 ):
     """Test a normal pw2gw.x output."""
     name = 'default'
@@ -21,21 +18,26 @@ def test_pw2gw_default(
 
     parser = generate_parser(entry_point_parser)
 
-    _, calcfunction = parser.parse_from_node(node, store_provenance=False)
+    results, calcfunction = parser.parse_from_node(node, store_provenance=False)
 
     assert calcfunction.is_finished, calcfunction.exception
     assert calcfunction.is_finished_ok, calcfunction.exit_message
     assert not orm.Log.objects.get_logs_for(node)
 
+    data_regression.check({'output_parameters': results['output_parameters'].get_dict()},
+                          basename='test_pw2gw_default_data')
 
-def test_pw2gw_failed_missing(
+    num_regression.check({k: v for k, v in results['eps'].get_iterarrays()}, basename='test_pw2gw_default_eps')
+
+
+def test_pw2gw_failed_missing_output(
     aiida_profile,
     fixture_localhost,
     generate_parser,
     generate_calc_job_node,
 ):
     """Test a pw2gw.x output where file are missing."""
-    name = 'failed_missing'
+    name = 'failed_missing_output'
     entry_point_calc_job = 'quantumespresso.pw2gw'
     entry_point_parser = 'quantumespresso.pw2gw'
 
@@ -48,6 +50,29 @@ def test_pw2gw_failed_missing(
     assert calcfunction.is_finished, calcfunction.exception
     assert calcfunction.is_failed, calcfunction.exit_status
     assert calcfunction.exit_status, node.process_class.exit_codes.ERROR_OUTPUT_FILES.status
+    assert orm.Log.objects.get_logs_for(node)
+
+
+def test_pw2gw_failed_missing_stdout(
+    aiida_profile,
+    fixture_localhost,
+    generate_parser,
+    generate_calc_job_node,
+):
+    """Test a pw2gw.x output where file are missing."""
+    name = 'failed_missing_stdout'
+    entry_point_calc_job = 'quantumespresso.pw2gw'
+    entry_point_parser = 'quantumespresso.pw2gw'
+
+    node = generate_calc_job_node(entry_point_calc_job, fixture_localhost, name)
+
+    parser = generate_parser(entry_point_parser)
+
+    _, calcfunction = parser.parse_from_node(node, store_provenance=False)
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_failed, calcfunction.exit_status
+    assert calcfunction.exit_status, node.process_class.exit_codes.ERROR_OUTPUT_STDOUT_MISSING.status
     assert orm.Log.objects.get_logs_for(node)
 
 
