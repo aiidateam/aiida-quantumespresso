80f49571efa7475d346dec50a9b91fd7565e7f6c
ConradJohnston <40352432+ConradJohnston@users.noreply.github.com>
2020-04-29 13:55:42 +0100

    Add `PpParser` for the `PpCalculation` plugin (#428)
    One challenge of pp.x calculations is that there is a choice of both
    dimensionality and of output format. As we want to produce AiiDA
    `ArrayData` output nodes, the `PpCalculation` plugin is modified to
    enforce only Gnuplot (for 1D and 2D) and Cube (3D only) file formats,
    based on the dimensionality the user wants. The `PpCalculation` class is
    still lightweight in the sense that the user skill is still required to
    run pp.x and hand-holding is minimal, but is improved over the previous
    'free-form' input version in that the output will definitely be parsed
    by AiiDA and stored in the database in a standard way. The parser
    collects the useful data from standard out and detects common problems.
    or convenience `PpCalculation` also enforces that the post-processed
    data is written to a file which is then retrieved and parsed, rather
    than to stdout. The parser converts this, for any dimensionality into
    the appropriate `ArrayData` representation.
    
    Co-authored-by: Sebastiaan Huber <mail@sphuber.net>

---
+++ tests/calculations/test_pp.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=redefined-outer-name
+"""Tests for the `PpCalculation` class."""
+from __future__ import absolute_import
+import pytest
+
+from aiida import orm
+from aiida.common import datastructures, AttributeDict
+
+
+@pytest.fixture
+def generate_inputs(fixture_localhost, fixture_sandbox, fixture_code, generate_remote_data):
+    """Return only those inputs that the parser will expect to be there."""
+
+    def _generate_inputs(parameters=None):
+        from aiida_quantumespresso.utils.resources import get_default_options
+
+        if parameters is None:
+            parameters = {'INPUTPP': {'plot_num': 1}, 'PLOT': {'iflag': 3}}
+
+        return AttributeDict({
+            'code':
+            fixture_code('quantumespresso.pp'),
+            'parent_folder':
+            generate_remote_data(fixture_localhost, fixture_sandbox.abspath, 'quantumespresso.pw'),
+            'parameters':
+            orm.Dict(dict=parameters),
+            'metadata': {
+                'options': get_default_options()
+            }
+        })
+
+    return _generate_inputs
+
+
+def test_pp_default(aiida_profile, fixture_sandbox, generate_calc_job, generate_inputs, file_regression):
+    """Test a default `PpCalculation`."""
+    entry_point_name = 'quantumespresso.pp'
+    inputs = generate_inputs()
+
+    calc_info = generate_calc_job(fixture_sandbox, entry_point_name, inputs)
+
+    retrieve_list = ['aiida.out']
+    retrieve_temporary_list = ['aiida.fileout']
+    local_copy_list = []
+
+    # Check the attributes of the returned `CalcInfo`
+    assert isinstance(calc_info, datastructures.CalcInfo)
+    assert sorted(calc_info.local_copy_list) == sorted(local_copy_list)
+    assert sorted(calc_info.retrieve_list) == sorted(retrieve_list)
+    assert sorted(calc_info.retrieve_temporary_list) == sorted(retrieve_temporary_list)
+
+    with fixture_sandbox.open('aiida.in') as handle:
+        input_written = handle.read()
+
+    # Checks on the files written to the sandbox folder as raw input
+    assert sorted(fixture_sandbox.get_content_list()) == sorted(['aiida.in'])
+    file_regression.check(input_written, encoding='utf-8', extension='.in')
+
+
+def test_pp_keep_plot_file(aiida_profile, fixture_sandbox, generate_calc_job, generate_inputs):
+    """Test a `PpCalculation` where we want to retrieve the plot file."""
+    entry_point_name = 'quantumespresso.pp'
+    inputs = generate_inputs()
+    inputs.metadata.options.keep_plot_file = True
+
+    calc_info = generate_calc_job(fixture_sandbox, entry_point_name, inputs)
+    retrieve_list = ['aiida.out', 'aiida.fileout']
+    retrieve_temporary_list = []
+    local_copy_list = []
+
+    # Check the attributes of the returned `CalcInfo`, no need to check the input file as it is not affected
+    assert isinstance(calc_info, datastructures.CalcInfo)
+    assert sorted(calc_info.local_copy_list) == sorted(local_copy_list)
+    assert sorted(calc_info.retrieve_list) == sorted(retrieve_list)
+    assert sorted(calc_info.retrieve_temporary_list) == sorted(retrieve_temporary_list)
+
+
+@pytest.mark.parametrize(
+    ('parameters', 'message'),
+    (
+        ({}, 'parameter `INPUTPP.plot_num` must be explicitly set'),
+        ({'INPUTPP': {}}, 'parameter `INPUTPP.plot_num` must be explicitly set'),
+        ({'INPUTPP': {'plot_num': 'str'}}, '`INTPUTPP.plot_num` must be an integer in the range'),
+        ({'INPUTPP': {'plot_num': 14}}, '`INTPUTPP.plot_num` must be an integer in the range'),
+        ({'INPUTPP': {'plot_num': 1}}, 'parameter `PLOT.iflag` must be explicitly set'),
+        ({'INPUTPP': {'plot_num': 1}, 'PLOT': {}}, 'parameter `PLOT.iflag` must be explicitly set'),
+        ({'INPUTPP': {'plot_num': 1}, 'PLOT': {'iflag': 'str'}}, '`PLOT.iflag` must be an integer in the range 0-4'),
+        ({'INPUTPP': {'plot_num': 1}, 'PLOT': {'iflag': 5}}, '`PLOT.iflag` must be an integer in the range 0-4'),
+    ),
+)  # yapf: disable
+def test_pp_invalid_parameters(aiida_profile, fixture_sandbox, generate_calc_job, generate_inputs, parameters, message):
+    """Test that launching `PpCalculation` fails for invalid parameters."""
+    entry_point_name = 'quantumespresso.pp'
+
+    with pytest.raises(ValueError) as exception:
+        inputs = generate_inputs(parameters=parameters)
+        generate_calc_job(fixture_sandbox, entry_point_name, inputs)
+
+    assert message in str(exception.value)
