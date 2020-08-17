d139591c71af0e9b984dd85d59525323e279c845
Lukas Botsch <lukas.botsch@gmail.com>
2020-08-17 09:52:19 +0200

    Fix parser regex

--- aiida_quantumespresso/parsers/projwfc.py
+++ aiida_quantumespresso/parsers/projwfc.py
@@ -51,16 +51,16 @@ def find_orbitals_from_statelines(out_info_dict):
         # spinorbit
         lnum_re = re.compile(r'l=\s*([0-9]+?)[^0-9]')
         jnum_re = re.compile(r'j=\s*([0-9.]+?)[^0-9.]')
-        mjnum_re = re.compile(r'm_j=\s*([0-9]+?)[^0-9.]')
+        mjnum_re = re.compile(r'm_j=\s*([-0-9.]+?)[^-0-9.]')
     elif not out_info_dict['collinear']:
         # non-collinear
         lnum_re = re.compile(r'l=\s*([0-9]+?)[^0-9]')
-        mnum_re = re.compile(r'm=\s*([0-9]+?)[^0-9]')
-        sznum_re = re.compile(r's_z=\s*([0-9.]*?)[^0-9.]')
+        mnum_re = re.compile(r'm=\s*([-0-9]+?)[^-0-9]')
+        sznum_re = re.compile(r's_z=\s*([-0-9.]*?)[^-0-9.]')
     else:
         # collinear / no spin
         lnum_re = re.compile(r'l=\s*([0-9]+?)[^0-9]')
-        mnum_re = re.compile(r'm=\s*([0-9]+?)[^0-9]')
+        mnum_re = re.compile(r'm=\s*([-0-9]+?)[^-0-9]')
     wfc_lines = out_info_dict['wfc_lines']
     state_lines = [out_file[wfc_line] for wfc_line in wfc_lines]
     state_dicts = []
