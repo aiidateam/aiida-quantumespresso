564aefdece7622ecd688e79239968b9278cddfbd
Davide Grassano <34096612+Crivella@users.noreply.github.com>
2021-10-04 08:28:04 +0200

    `PwRelaxWorkChain` do not override `SYSTEM/nbnd` in final scf if set. (#708)
    As of now, the workchain overrides the `SYSTEM/nbnd` input given for the
    parameters of the `base_final_scf` namespace with the number of bands
    read from the output of the relax calculation. However, in certain cases
    the user may wants to use more bands (e.g.: relax without SOC and scf
    with SOC) which will now causes the calculation to fail with the error
    "too few bands". Here we change the workchain to only override the input
    parameter if it has not been explicitly defined in the workchain inputs.

--- aiida_quantumespresso/workflows/pw/relax.py
+++ aiida_quantumespresso/workflows/pw/relax.py
@@ -338,7 +338,8 @@ class PwRelaxWorkChain(ProtocolMixin, WorkChain):
         inputs = self.ctx.final_scf_inputs
         inputs.pw.structure = self.ctx.current_structure
 
-        if self.ctx.current_number_of_bands is not None:
+        inputs_nbnd = inputs.pw.parameters.get('SYSTEM', {}).get('nbnd', None)
+        if self.ctx.current_number_of_bands is not None and inputs_nbnd is None:
             inputs.pw.parameters.setdefault('SYSTEM', {})['nbnd'] = self.ctx.current_number_of_bands
 
         inputs = prepare_process_inputs(PwBaseWorkChain, inputs)
