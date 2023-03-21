50418c802633d134435d4bea3c88e43e17b20b5b
bastonero <bastonero.lorenzo@gmail.com>
2023-03-21 12:37:15 +0000

    Feature: Hubbard data schema using `pydantic`

---
+++ src/aiida_quantumespresso/common/hubbard.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""Utility class and functions for HubbardStructureData."""
+from typing import List, Union
+
+from pydantic import BaseModel, ValidationError, conint, constr, validator
+
+allowed_types_left = ['Dudarev', 'Liechtenstein']
+allowed_types_right = ['U', 'V', 'J', 'Ueff', 'B', 'E2', 'E3']
+allowed_projectors = [
+    'atomic',
+    'ortho-atomic',
+    'norm-atomic',
+    'wannier-functions',
+    'pseudo-potentials',
+]
+
+
+class HubbardParameters(BaseModel):  # Or `HubbardInteraction` ?
+    """Class for describing onsite and intersite Hubbard interaction parameters."""
+    atom_index: conint(strict=True)
+    atom_manifold: constr(strip_whitespace=True, to_lower=True, min_length=2, max_length=5)
+    neighbour_index: conint(strict=True)
+    neighbour_manifold: constr(strip_whitespace=True, to_lower=True, min_length=2, max_length=5)
+    translation_vector: List[conint(strict=True)]
+    hubbard_value: float  # just `value`?
+    hubbard_type: str  # just `type`?
+
+    @validator('atom_index', 'neighbour_index')
+    def check_index(cls, value):
+        if value < 0:
+            raise ValueError('non integer index')
+        return value
+
+    @validator('translation_vector')
+    def check_translation_vector(cls, value):
+        if len(value) != 3:
+            raise ValueError('only 3 integers')
+        return value
+
+    @validator('atom_manifold', 'neighbour_manifold')
+    def check_manifolds(cls, value):
+        le = len(value)
+        if le not in [2, 5]:
+            raise ValueError(f'invalid length. Only 2 or 5.')
+        if le == 2:
+            if not value[0] in [str(_ + 1) for _ in range(6)]:
+                raise ValueError(f'invalid quantum number {value[0]}')
+            if not value[1] in ['s', 'p', 'd', 'f', 'h']:
+                raise ValueError(f'invalid manifold symbol {value[1]}')
+        if le == 5:
+            if not value[2] == '-':
+                raise ValueError(f'the separator {value[0]} is not allowed. Only `-`')
+            if not value[3] in [str(_ + 1) for _ in range(6)]:
+                raise ValueError(f'the quantum number {value[0]} is not correct')
+            if not value[4] in ['s', 'p', 'd', 'f', 'h']:
+                raise ValueError(f'the manifold number {value[1]} is not correct')
+        return value
+
+    @validator('hubbard_type')
+    def check_hubbard_type(cls, value):
+        message = (
+            'Hubbard type must be `{left}-{right}`, where the allowed values are:\n'\
+            '* left: '+', '.join(allowed_types_left)+'\n'
+            '* right: '+', '.join(allowed_types_right)
+        )
+        normalized = '-'.join((word.capitalize()) for word in value.split('-'))
+        left = normalized.split('-')[0]
+        try:
+            right = normalized.split('-')[1]
+        except IndexError:
+            raise ValidationError(message)
+        if left not in allowed_types_left or right not in allowed_types_right:
+            raise ValueError(message)
+
+        return normalized
+
+    def to_list(self):
+        return [
+            self.atom_index, self.atom_manifold, self.neighbour_index, self.neighbour_manifold, self.hubbard_value,
+            self.translation_vector, self.hubbard_type
+        ]
+
+    @classmethod
+    def from_list(cls, hubbard_parameters: list):
+        keys = [
+            'atom_index',
+            'atom_manifold',
+            'neighbour_index',
+            'neighbour_manifold',
+            'hubbard_value',
+            'translation_vector',
+            'hubbard_type',
+        ]
+        return cls(**{key: value for key, value in zip(keys, hubbard_parameters)})
+
+
+class HubbardProjectors(BaseModel):
+    """Class defining the Hubbard projectors."""
+    hubbard_projectors: str = 'ortho-atomic'  # only `projectors`?
+
+    @validator('hubbard_projectors')
+    def check_projectors(cls, value):
+        normalized = value.lower()
+        if normalized not in allowed_projectors:
+            raise ValueError('Hubbard projectors not in the allowed list: ' + ', '.join(allowed_projectors))
+        return normalized
+
+
+class Hubbard(HubbardProjectors):
+    """Class for complete description of Hubbard interactions."""
+    hubbard_parameters: List[HubbardParameters]  # only `parameters`?
+
+    def to_list(self):
+        return [hp.to_list() for hp in self.hubbard_parameters]
+
+    @classmethod
+    def from_list(cls, hubbard_parameters: List[List[Union[int, str, int, str, list, float, str]]]):
+        return cls(hubbard_parameters=[HubbardParameters.from_list(value) for value in hubbard_parameters])
---
+++ tests/common/test_hubbard.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""Tests for :py:mod:`~aiida_quantumespresso.common.hubbard`."""
+from pydantic import ValidationError
+import pytest
+
+from aiida_quantumespresso.common.hubbard import Hubbard, HubbardParameters, HubbardProjectors
+
+# Maybe we should define a Class for testing?
+valid_parameters = {
+    'atom_index': 0,
+    'atom_manifold': '3d',
+    'neighbour_index': 1,
+    'neighbour_manifold': '2p',
+    'translation_vector': [0, 0, 0],
+    'hubbard_value': 5.0,
+    'hubbard_type': 'Dudarev-U',
+}
+
+
+@pytest.fixture
+def get_hubbard_parameters():
+    """Return an `HubbardParameters` intstance."""
+
+    def _get_hubbard_parameters(overrides=None):
+        """Return an `HubbardParameters` intstance."""
+        inputs = valid_parameters.copy()
+
+        if overrides:
+            inputs.update(overrides)
+
+        return HubbardParameters(**inputs)
+
+    return _get_hubbard_parameters
+
+
+@pytest.fixture
+def get_hubbard():
+    """Return an `Hubbard` intstance."""
+
+    def _get_hubbard():
+        """Return an `Hubbard` intstance."""
+        hp = HubbardParameters(**valid_parameters)
+
+        return Hubbard(hubbard_parameters=[hp, hp])
+
+    return _get_hubbard
+
+
+def test_valid_hubbard_projectors():
+    """Test valid inputs for py:meth:`~aiida_quantumespresso.common.hubbard.HubbardProjectors`."""
+    from aiida_quantumespresso.common.hubbard import allowed_projectors
+    for projectors in allowed_projectors:
+        hubbard_projectors = HubbardProjectors(hubbard_projectors=projectors)
+        assert hubbard_projectors.hubbard_projectors == projectors
+
+
+def test_invalid_hubbard_projectors():
+    """Test valid inputs for py:meth:`~aiida_quantumespresso.common.hubbard.HubbardProjectors`."""
+    invalid_name = 'invalid-name'
+    with pytest.raises(ValidationError):
+        HubbardProjectors(hubbard_projectors=invalid_name)
+
+
+def test_safe_hubbard_parameters(get_hubbard_parameters):
+    """Test valid inputs are stored correctly for py:meth:`~aiida_quantumespresso.common.hubbard.HubbardParameters`."""
+    hp_dict = get_hubbard_parameters().dict()
+    assert hp_dict == valid_parameters
+
+
+def test_from_to_list_parameters(get_hubbard_parameters):
+    """Test py:meth:`~aiida_quantumespresso.common.hubbard.HubbardParameters.to_list` and
+    py:meth:`~aiida_quantumespresso.common.hubbard.HubbardParameters.from_list`."""
+    hp = get_hubbard_parameters()
+    hp_list = [0, '3d', 1, '2p', 5.0, [0, 0, 0], 'Dudarev-U']
+    assert hp.to_list() == hp_list
+    hp = HubbardParameters.from_list(hp_list)
+    assert hp.dict() == valid_parameters
+
+
+@pytest.mark.parametrize(
+    'overrides',
+    [
+        {
+            'atom_index': 0
+        },  # making sure is valid - some validators (e.g. PositiveInt) do not accept it
+        {
+            'atom_manifold': '3d-2p'
+        },
+        {
+            'translation_vector': [0, -1, +1]
+        },  # this type also valid
+        {
+            'hubbard_type': 'Liechtenstein-B'
+        }
+    ]
+)
+def test_valid_hubbard_parameters(get_hubbard_parameters, overrides):
+    """Test valid inputs for py:meth:`~aiida_quantumespresso.common.hubbard.HubbardParameters`."""
+    hp_dict = get_hubbard_parameters(overrides=overrides).dict()
+    new_dict = valid_parameters.copy()
+    new_dict.update(overrides)
+    assert hp_dict == new_dict
+
+
+@pytest.mark.parametrize(
+    'overrides', [
+        {
+            'atom_index': -1
+        },
+        {
+            'atom_index': 0.5
+        },
+        {
+            'atom_manifold': '3z'
+        },
+        {
+            'atom_manifold': '3d2p'
+        },
+        {
+            'atom_manifold': '3d-3p-2s'
+        },
+        {
+            'translation_vector': [0, 0]
+        },
+        {
+            'translation_vector': [0, 0, 0, 0]
+        },
+        {
+            'translation_vector': [0, 0, -1.5]
+        },
+        {
+            'hubbard_type': 'liechtenstein-L'
+        },
+        {
+            'hubbard_type': 'hubbard-L'
+        },
+        {
+            'hubbard_type': 'DudarevU'
+        },
+    ]
+)
+def test_invalid_hubbard_parameters(get_hubbard_parameters, overrides):
+    """Test invalid inputs for py:meth:`~aiida_quantumespresso.common.hubbard.HubbardParameters`."""
+    with pytest.raises(ValidationError):
+        get_hubbard_parameters(overrides=overrides)
+    # maybe we should add the check on the Exception info as well
+
+
+def test_from_to_list_hubbard(get_hubbard):
+    """Test py:meth:`~aiida_quantumespresso.common.hubbard.Hubbard.to_list` and
+    py:meth:`~aiida_quantumespresso.common.hubbard.Hubbard.from_list`."""
+    hubbard = get_hubbard()
+    hp_list = [0, '3d', 1, '2p', 5.0, [0, 0, 0], 'Dudarev-U']
+
+    hubbard_list = [hp_list, hp_list]
+    assert hubbard.to_list() == hubbard_list
+
+    hubbard = Hubbard.from_list(hubbard_list)
+    assert hubbard.dict() == {
+        'hubbard_parameters': [valid_parameters, valid_parameters],
+        'hubbard_projectors': 'ortho-atomic',
+    }
