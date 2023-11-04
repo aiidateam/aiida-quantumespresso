8b41937349ef68def5b3956aaadf2c4e67c15769
Jusong Yu <jusong.yeu@gmail.com>
2023-11-04 16:05:49 +0100

    Implement unit test for the built image

---
+++ .docker/tests/conftest.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring, redefined-outer-name
+import json
+import re
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture(scope='session', params=['aiida-quantumespresso'])
+def variant(request):
+    return request.param
+
+
+@pytest.fixture(scope='session')
+def docker_compose_file(pytestconfig, variant):  # pylint: disable=unused-argument
+    return f'docker-compose.{variant}.yml'
+
+
+@pytest.fixture(scope='session')
+def docker_compose(docker_services):
+    # pylint: disable=protected-access
+    return docker_services._docker_compose
+
+
+@pytest.fixture
+def timeout():
+    """Container and service startup timeout"""
+    return 60
+
+
+@pytest.fixture
+def container_user():
+    return 'aiida'
+
+
+@pytest.fixture
+def aiida_exec(docker_compose):
+
+    def execute(command, user=None, **kwargs):
+        if user:
+            command = f'exec -T --user={user} aiida {command}'
+        else:
+            command = f'exec -T aiida {command}'
+        return docker_compose.execute(command, **kwargs)
+
+    return execute
+
+@pytest.fixture
+def qe_version(aiida_exec):
+    info = json.loads(
+        aiida_exec(
+            "mamba list -n base --json --full-name qe"
+        ).decode()
+    )[0]
+    return info['version']
+
+@pytest.fixture
+def sssp_version(aiida_exec, container_user):
+    output = aiida_exec("aiida-pseudo list", user=container_user).decode().strip()
+    return re.search(r"SSSP/(\d+\.\d+)/PBE/efficiency", output).group(1)
---
+++ .docker/tests/test_aiida.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+import json
+import time
+
+from packaging.version import parse
+import pytest
+
+
+def test_verdi_status(aiida_exec, container_user, timeout):
+    time.sleep(timeout)
+    output = aiida_exec('verdi status', user=container_user).decode().strip()
+    assert 'Connected to RabbitMQ' in output
+    assert 'Daemon is running' in output
+
+    # check that we have suppressed the warnings
+    assert 'Warning' not in output
+
+
+def test_computer_setup_success(aiida_exec, container_user, timeout):
+    time.sleep(timeout)
+    output = aiida_exec('verdi computer test localhost', user=container_user).decode().strip()
+
+    assert "Success" in output
+    assert "Failed" not in output
+
+def test_run_real_pw_computation(aiida_exec, container_user, qe_version, sssp_version, timeout):
+    import re
+
+    time.sleep(timeout)
+    output = aiida_exec("verdi data core.structure import ase /opt/examples/Si.cif", user=container_user).decode().strip()
+    
+    # Find pk 
+    pk = re.search(r"PK = (\d+)", output).group(1)
+
+    cmd = f"aiida-quantumespresso calculation launch pw -X pw-{qe_version}@localhost -F SSSP/{sssp_version}/PBE/efficiency -S {pk} -k 1 1 1"
+    output = aiida_exec(cmd, user=container_user).decode().strip()
+
+    assert "terminated with state: finished [0]" in output
