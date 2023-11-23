39287e03cb6bbf1915662685a5c441e9c7c36030
bastonero <bastonero.lorenzo@gmail.com>
2023-11-23 17:10:14 +0000

    Test/Improve `PhBaseWorkChain` overrides/protocol
    The `PhBaseWorkChain.get_builder_from_protocol` was untested.
    Tests are added, and the `electronic_type` input is added to allow
    for the computation of the dielectric and effective charge tensors
    when an insulator is in input. This is useful in other workchains, so
    that the `electronic_type` input can be passed seamlessly as a
    kwargs input at an higher level.

--- src/aiida_quantumespresso/workflows/ph/base.py
+++ src/aiida_quantumespresso/workflows/ph/base.py
@@ -9,6 +9,7 @@ from aiida.engine import BaseRestartWorkChain, ProcessHandlerReport, process_han
 from aiida.plugins import CalculationFactory
 
 from aiida_quantumespresso.calculations.functions.merge_ph_outputs import merge_ph_outputs
+from aiida_quantumespresso.common.types import ElectronicType
 from aiida_quantumespresso.workflows.protocols.utils import ProtocolMixin
 
 PhCalculation = CalculationFactory('quantumespresso.ph')
@@ -64,7 +65,16 @@ class PhBaseWorkChain(ProtocolMixin, BaseRestartWorkChain):
         return files(ph_protocols) / 'base.yaml'
 
     @classmethod
-    def get_builder_from_protocol(cls, code, parent_folder=None, protocol=None, overrides=None, options=None, **_):
+    def get_builder_from_protocol(
+        cls,
+        code,
+        parent_folder=None,
+        protocol=None,
+        overrides=None,
+        electronic_type=ElectronicType.METAL,
+        options=None,
+        **_
+    ):
         """Return a builder prepopulated with inputs selected according to the chosen protocol.
 
         :param code: the ``Code`` instance configured for the ``quantumespresso.ph`` plugin.
@@ -73,6 +83,7 @@ class PhBaseWorkChain(ProtocolMixin, BaseRestartWorkChain):
         :param overrides: optional dictionary of inputs to override the defaults of the protocol.
         :param options: A dictionary of options that will be recursively set for the ``metadata.options`` input of all
             the ``CalcJobs`` that are nested in this work chain.
+        :param electronic_type: indicate the electronic character of the system through ``ElectronicType`` instance.
         :return: a process builder instance with all inputs defined ready for launch.
         """
         from aiida_quantumespresso.workflows.protocols.utils import recursive_merge
@@ -81,8 +92,15 @@ class PhBaseWorkChain(ProtocolMixin, BaseRestartWorkChain):
             code = orm.load_code(code)
 
         type_check(code, orm.AbstractCode)
+        type_check(electronic_type, ElectronicType)
+
+        if electronic_type not in [ElectronicType.METAL, ElectronicType.INSULATOR]:
+            raise NotImplementedError(f'electronic type `{electronic_type}` is not supported.')
 
         inputs = cls.get_protocol_inputs(protocol, overrides)
+
+        if electronic_type is ElectronicType.INSULATOR:
+            inputs['ph']['parameters']['INPUTPH']['epsil'] = True
 
         qpoints_mesh = inputs['ph'].pop('qpoints')
         qpoints = orm.KpointsData()
@@ -104,6 +122,7 @@ class PhBaseWorkChain(ProtocolMixin, BaseRestartWorkChain):
             builder.ph['settings'] = orm.Dict(inputs['ph']['settings'])
         builder.clean_workdir = orm.Bool(inputs['clean_workdir'])
         builder.ph['qpoints'] = qpoints
+        builder.max_iterations = orm.Int(inputs['max_iterations'])
         # pylint: enable=no-member
 
         return builder
