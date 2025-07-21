5586c3fee9bd4118c7a61b52b913540db356832e
jiang-yuha0 <149360440+jiang-yuha0@users.noreply.github.com>
2025-07-21 14:23:16 +0800

    ✨ Protocols: Add support for non-collinear calculations
    Replaces the `get_starting_magnetization` utility function for the protocols by the
    `get_magnetization` one, which now also accepts the `spin_type` input to deal with
    both collinear and non-collinear calculations. The new function returns a single
    dictionary with three keys:
    
    * `starting_magnetization`
    * `angle1`
    * `angle2`
    
    Corresponding to the Quantum ESPRESSO inputs. For `spin_type` = `SpinType.COLLINEAR`,
    the angles are set to `None`. Since the API of this function is different, we leave
    the old `get_starting_magnetization` function and deprecate it.
    
    The function is also able to read magnetic moment from structure of type
    `MagneticStructureData`, implemented in `aiida_wannier90_workflows`. Since we do not
    want to make this package a dependency, we cannot add tests for this part of the
    functionality. The corresponding code is also clearly isolated so it can be removed,
    e.g. in favor of the new `StructureData` class from `aiida_atomistic` that would also
    host magnetic properties, but is currently not stable yet.
    
    Additionally, the `Pw2wannier90Calculation` has been modified to handle cases where a
    single element exhibits multiple different magnetic moments, when we have to name them
    with different labels. To address this, when using external projectors, the projector
    files are now copied to the working directory and renamed to match the corresponding
    labels, ensuring compatibility with the projector requirements in `pw2wannier90.x`.
    
    Co-authored-by: Marnik Bercx <mbercx@gmail.com>

--- tests/workflows/protocols/test_utils.py
+++ tests/workflows/protocols/test_utils.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 """Tests for the utility functions for the protocols."""
 
+import pytest
+
+from aiida_quantumespresso.common.types import SpinType
+
 
 def test_recursive_merge():
     """Test the recursive merge function."""
@@ -13,3 +17,121 @@ def test_recursive_merge():
     assert right_dict == {'a': {'c': {'d': 'D'}}, 'e': {'f': 'F'}}
 
     assert merged == {'a': {'b': 1, 'c': {'d': 'D'}}, 'e': {'f': 'F'}, 'g': 3}
+
+
+# Note: for the `sssp` text fixture, the z_valence is 4 for every element.
+# This can lead to some nonsensical magnetizations, but that is fine for testing
+# purposes.
+@pytest.mark.parametrize(
+    'structure_id,initial_magnetic_moments,spin_type,expected_magnetization',
+    (
+        (
+            'silicon',
+            None,
+            SpinType.COLLINEAR,
+            {
+                'starting_magnetization': {
+                    'Si': 0.1
+                },
+                'angle1': None,
+                'angle2': None
+            },
+        ),
+        (
+            'cobalt-prim',
+            None,
+            SpinType.COLLINEAR,
+            {
+                'starting_magnetization': {
+                    'Co': 1.25
+                },
+                'angle1': None,
+                'angle2': None
+            },
+        ),
+        (
+            'cobalt-prim',
+            None,
+            SpinType.NON_COLLINEAR,
+            {
+                'starting_magnetization': {
+                    'Co': 1.25
+                },
+                'angle1': {
+                    'Co': 0
+                },
+                'angle2': {
+                    'Co': 0
+                }
+            },
+        ),
+        (
+            'cobalt-prim',
+            {
+                'Co': 3
+            },
+            SpinType.COLLINEAR,
+            {
+                'starting_magnetization': {
+                    'Co': 0.75
+                },
+                'angle1': None,
+                'angle2': None
+            },
+        ),
+        (
+            'cobalt-prim',
+            {
+                'Co': (1, 2, 3)
+            },
+            SpinType.NON_COLLINEAR,
+            {
+                'starting_magnetization': {
+                    'Co': 0.25
+                },
+                'angle1': {
+                    'Co': 2
+                },
+                'angle2': {
+                    'Co': 3
+                }
+            },
+        ),
+    ),
+)
+def test_get_magnetization(
+    sssp,
+    generate_structure,
+    structure_id,
+    initial_magnetic_moments,
+    spin_type,
+    expected_magnetization,
+):
+    """Test the `get_magnetization` function."""
+    from aiida_quantumespresso.workflows.protocols.utils import get_magnetization
+
+    magnetization = get_magnetization(generate_structure(structure_id), sssp, initial_magnetic_moments, spin_type)
+
+    assert magnetization == expected_magnetization
+
+
+@pytest.mark.parametrize(
+    'structure_id,initial_magnetic_moments,spin_type,expected_error,error_message',
+    (
+        ('silicon', {}, SpinType.COLLINEAR, ValueError, '`initial_magnetic_moments` needs one value for each of'),
+        ('silicon', {
+            'Si': (1, 2, 3)
+        }, SpinType.COLLINEAR, TypeError, 'Spin type is set to '),
+        ('silicon', {
+            'Si': 'zero'
+        }, SpinType.COLLINEAR, TypeError, 'Unrecognised type for magnetic moment'),
+    ),
+)
+def test_get_magnetization_failure(
+    sssp, generate_structure, structure_id, initial_magnetic_moments, spin_type, expected_error, error_message
+):
+    """Test the `get_magnetization` function."""
+    from aiida_quantumespresso.workflows.protocols.utils import get_magnetization
+
+    with pytest.raises(expected_error, match=error_message):
+        get_magnetization(generate_structure(structure_id), sssp, initial_magnetic_moments, spin_type)
