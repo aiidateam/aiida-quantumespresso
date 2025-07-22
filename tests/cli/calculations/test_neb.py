ec8bbc614a880a90b6678beb9bb403a7aff7b3b4
Marnik Bercx <mbercx@gmail.com>
2025-07-22 10:31:25 +1000

    🧪 Extend `sssp` fixture to PseudoDojo
    Add a `PseudoDojoFamily` to the `sssp` fixture to allow testing with this type of
    pseudopotential family, e.g. for spin-orbit calculations. Since the fixture is now not
    only providing `SsspFamily` pseudo potentials, we rename it to `pseudo_family`.
    
    The cutoffs are set to a distinct value so tests can check for this when the
    `PseudoDojoFamily` is meant to be used.

--- tests/cli/calculations/test_neb.py
+++ tests/cli/calculations/test_neb.py
@@ -3,9 +3,9 @@
 from aiida_quantumespresso.cli.calculations.neb import launch_calculation
 
 
-def test_command_base(run_cli_process_launch_command, fixture_code, sssp, generate_structure):
+def test_command_base(run_cli_process_launch_command, fixture_code, pseudo_family, generate_structure):
     """Test invoking the calculation launch command with only required inputs."""
     code = fixture_code('quantumespresso.neb').store()
     structures = [generate_structure().store().pk, generate_structure().store().pk]
-    options = ['-X', code.full_label, '-F', sssp.label, '-s'] + structures
+    options = ['-X', code.full_label, '-F', pseudo_family.label, '-s'] + structures
     run_cli_process_launch_command(launch_calculation, options=options)
