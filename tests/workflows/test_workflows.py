aa1cb7407e2c63debb19c98009173ab4f1267c28
Marnik Bercx <mbercx@gmail.com>
2025-08-29 11:46:28 +1000

    🧪 Test unrecoverable failures for `BaseRestartWorkChain`s
    The `BaseRestartWorkChain` has a feature that the first time it encounters an unhandled
    error, it restarts the process. However, in case the process fails twice consecutively
    with an unhandled error, the `BaseRestartWorkChain` is finished with an
    `ERROR_SECOND_CONSECUTIVE_UNHANDLED_FAILURE`. This is to deal with transient errors,
    for example in the case of node failure.
    
    Here we add a test to verify this behaviour for the `BaseRestartWorkChain`s.

---
+++ tests/workflows/test_workflows.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""General tests for workflows."""
+from aiida.engine import CalcJob
+from plumpy import ProcessState
+import pytest
+
+
+@pytest.mark.parametrize(
+    'entry_point', (
+        'quantumespresso.bands.base',
+        'quantumespresso.matdyn.base',
+        'quantumespresso.pw.base',
+        'quantumespresso.q2r.base',
+        'quantumespresso.xspectra.base',
+    )
+)
+def test_base_unrecoverable_failure(generate_workchain, generate_calc_job_node, generate_inputs, entry_point):
+    """Test that the `BaseRestartWorkChain` workflows handle generic unrecoverable failures properly."""
+    exit_code = CalcJob.exit_codes.ERROR_SCHEDULER_NODE_FAILURE
+
+    calcjob_entry_point = entry_point.removesuffix('.base')
+    calcjob_namespace = calcjob_entry_point.split('.')[-1]
+    process = generate_workchain(entry_point, {calcjob_namespace: generate_inputs(calcjob_entry_point)})
+    process.setup()
+
+    node = generate_calc_job_node()
+    node.set_process_state(ProcessState.FINISHED)
+    node.set_exit_status(exit_code.status)
+
+    process.ctx.children = [node]
+    process.ctx.iteration = 1
+
+    result = process.inspect_process()
+    assert result is None, 'The first inspection should return `None`, i.e. the `BaseRestartWorkChain` should continue'
+
+    node = generate_calc_job_node()
+    node.set_process_state(ProcessState.FINISHED)
+    node.set_exit_status(exit_code.status)
+
+    process.ctx.children.append(node)
+    process.ctx.iteration = 2
+
+    assert process.inspect_process() == process.exit_codes.ERROR_SECOND_CONSECUTIVE_UNHANDLED_FAILURE, \
+        'The second inspection should return the proper exit code, i.e. the `BaseRestartWorkChain` should stop'
