afef0a679212e21c4956a2007a48ba307aa08bd3
t-reents <timo.reents1@gmail.com>
2025-09-09 14:59:26 +0200

    First draft of a command to automatically setup (s) for QE executables

--- src/aiida_quantumespresso/cli/setup/setup_cmd.py
+++ src/aiida_quantumespresso/cli/setup/setup_cmd.py
@@ -2,6 +2,7 @@
 """Command line interface commands for setting up `code`s for Quantum ESPRESSO executables."""
 # -*- coding: utf-8 -*-
 import pathlib
+import re
 
 from aiida import orm
 from aiida.cmdline.params import arguments
@@ -11,25 +12,102 @@ from aiida.common.exceptions import NotExistent
 import click
 
 
-# add option to specify executables, otherwise try to set all that can be found in the directory
-# documentation: this is equivalen to: list of executables e.g. pw.x dos. projwfc.x ...
-
-# suffix: try to use curly brackets and f-strings {}_, show example in docstrings
-
-# version in the name
-# try to run verdi code test and parse version number
-# --test option to run the test of code
-# maybe, automatic test does only work if we don't submit
-# just use transport to run code
-
 @arguments.COMPUTER()
-@click.argument('executables', nargs=-1, required=True)
+@click.argument(
+    'executables',
+    nargs=-1,
+    required=True,
+)
 @click.option('--directory', '-d', help='Absolute path to directory where the executbale(s) is (are) located.')
-@click.option('--label_suffix', '-l', help='Suffix to append to the label of the code instance.', default='')
+@click.option(
+    '--label-template',
+    '-l',
+    help=(
+        'Label template of the code instance. Use curly brackets to reference '
+        'the executable label, e.g. `qe-{}` will create a `Code` with label '
+        '`qe-pw` for `pw.x`.'
+    ),
+    default=''
+)
 @options_code.PREPEND_TEXT()
 @options_code.APPEND_TEXT()
-def create_code(executables, computer, directory, label_suffix, **kwargs):
-    """Create a `code` instance for a Quantum ESPRESSO executable."""
+def create_code(computer, executables, directory, label_template, **kwargs):
+    """Create a `orm.Code` instance for Quantum ESPRESSO executables.
+
+    Specify a single executable or a list of Quantum ESPRESSO executables to
+    create codes for. You can provide multiple executables separated by
+    space, e.g. `pw.x dos.x`.
+
+    Specify a directory where the executables are located with `--directory` or
+    `-d`. If not provided, the command will try to find the executables in
+    the `PATH` on the remote computer. Consider adding a prepend text if modules
+    need to be loaded to find the executables.
+    """
+    prepend_text = kwargs.pop('prepend_text', None)
+    append_text = kwargs.pop('append_text', None)
+
+    executable_paths = _get_executable_paths(
+        prepend_text=prepend_text, executables=executables, computer=computer, directory=directory, **kwargs
+    )
+
+    for executable, exec_path in executable_paths:
+        existing_label, label = _get_code_label(label_template=label_template, executable=executable, computer=computer)
+
+        if existing_label:
+            echo.echo_warning(f'Code with label<{existing_label}> already exists on Computer<{computer.label}>.')
+            if not click.confirm(f'Do you want to add another instance with label {label}?'):
+                continue
+
+        code = orm.InstalledCode(
+            label=label,
+            computer=computer,
+            filepath_executable=exec_path,
+            default_calc_job_plugin=f'quantumespresso.{executable.split(".")[0]}',
+            prepend_text=prepend_text,
+            append_text=append_text
+        )
+        code.store()
+        echo.echo_success(
+            f'Code<{code.label}> for {executable} created with pk<{code.pk}> on Computer<{computer.label}>'
+        )
+
+
+def _get_code_label(label_template: str, executable: str, computer: orm.Computer) -> tuple[str | None, str]:
+    existing_label = None
+    label = label_template.replace('{}', executable.replace('.x',
+                                                            '')) if label_template else executable.replace('.x', '')
+
+    # Check if code already exists
+    existing_labels = orm.QueryBuilder().append(orm.Computer, filters={
+        'label': computer.label
+    }, tag='computer').append(
+        orm.InstalledCode,
+        with_computer='computer',
+        filters={
+            'label': {
+                'like': f'{label}%'
+            }
+        },
+        tag='code',
+        project='label'
+    ).all(flat=True)
+    pattern = re.compile(rf'^{re.escape(label)}(-\d+)?$')
+    n_existing_codes = len([l for l in existing_labels if pattern.match(l)])
+
+    if n_existing_codes > 0:
+        existing_label = label
+        label = f'{label}-{n_existing_codes+1}'
+
+    return existing_label, label
+
+
+def _get_executable_paths(
+    prepend_text: str,
+    executables: str | list[str],
+    computer: orm.Computer,
+    directory: str,
+) -> list[tuple[str, str]]:
+    """Return the absolute paths of the executables on the given computer."""
     user = orm.User.collection.get_default()
 
     try:
@@ -43,7 +121,6 @@ def create_code(executables, computer, directory, label_suffix, **kwargs):
 
     transport = authinfo.get_transport()
 
-    prepend_text = kwargs.pop('prepend_text', None)
     executable_paths = []
     with transport:
         for executable in executables:
@@ -55,73 +132,64 @@ def create_code(executables, computer, directory, label_suffix, **kwargs):
                 which_ret_val, exec_path, which_stderr = transport.exec_command_wait(f'which {executable}')
 
                 if which_ret_val != 0:
-                    raise ValueError(
+                    echo.echo_error(
                         f'Failed to determine the path of the executable<{executable}> on '
-                        f'computer<{computer.label}>: {which_stderr}'
+                        f'computer<{computer.label}>:\n\t{which_stderr}'
                     )
+                    continue
             else:
                 directory = pathlib.PurePosixPath(directory)
                 if not directory.is_absolute():
-                    raise ValueError(f'Directory<{directory}> is not an absolute path.')
+                    echo.echo_error(f'Directory<{directory}> is not an absolute path.')
+                    continue
                 if not transport.path_exists(directory):
-                    raise ValueError(f'Directory<{directory}> does not exist on computer<{computer.label}>')
+                    echo.echo_error(f'Directory<{directory}> does not exist on computer<{computer.label}>')
+                    continue
                 exec_path = directory / executable
             # also check that executable is executable and exists
-            executable_paths.append(str(exec_path))
+            executable_paths.append((executable, str(exec_path)))
 
-    for executable, exec_path in zip(executables, executable_paths):
-        label = f'{executable.replace(".x", "")}_{label_suffix}' if label_suffix else executable.replace(".x", "")
-        code = orm.InstalledCode(
-            label=label,
-            computer=computer,
-            filepath_executable=exec_path,
-            default_calc_job_plugin=f'quantumespresso.{executable.split(".")[0]}',
-            prepend_text=prepend_text
+    return executable_paths
+
+
+# This is just an example of how a test command to test a
+# PW code could look like. One should rethink the submission, e.g., which queue
+# and resources to be used
+
+
+@click.argument('codes', nargs=-1, required=True)
+def test_code(codes):
+    """Test a `code` instance for a Quantum ESPRESSO executable."""
+    from aiida.engine import submit
+    from aiida.orm import load_code
+    from ase.build import bulk
+
+    from aiida_quantumespresso.workflows.pw.base import PwBaseWorkChain
+
+    # Create a silicon crystal structure
+    structure = bulk('Si')
+    structure = orm.StructureData(ase=structure)
+    assert isinstance(codes, (list, tuple))
+    for code_label in codes:
+        code = load_code(code_label)
+        plugin = code.default_calc_job_plugin.split('.')[-1]
+        if plugin != 'pw':
+            echo.echo_warning(f'Code<{code.label}> is not a pw.x code, skipping test.')
+            continue
+        # Create a Quantum ESPRESSO calculation
+        builder = PwBaseWorkChain.get_builder_from_protocol(
+            code=code,
+            structure=structure,
+            protocol='fast',
+            overrides={'pw': {
+                'parameters': {
+                    'ELECTRONS': {
+                        'electron_maxstep': 2,
+                        'scf_must_converge': False
+                    }
+                }
+            }}
         )
-        code.store()
-        echo.echo_success(f'Code<{code.label}> for {executable} created with pk<{code.pk}> on computer<{computer.label}>')
-
-# add option --no-connect, makes -d mandatory
-# In case you can not connect
-
-# Run test, start code
-# parse general information of the code e.g. simply run `pw.x` and append to extras
-# extras can also be used to validate if things change
-
-# aiida-quantumespresso code test
-    # run on login node
-    # --via-submit --> check report when it's done
-    # set extras on the code in the end
-    
-# set extras, option -- don't set extras
-    # md5 of the file
-    # version of the code
-    # compiled features? e.g. scalapack, hdf5, ...
-    # If md5 changes, fails --> optional force extras update
-    # Fails if others change as well
-    
-# test multiple codes
-# use similar template from setup to run multiple codes that match the same pattern
-# unless disabled, will also test code version and add it to the extras
-# --provide-relabel-command/relabel-pattern=
-# final report will show how to relabel codes based on the found information
-# make it easily accessible to copy and paste
-
-# relabel in interactive mode --relabel, -n option
-# What to do if code with given label already exists?
-# report existing codes and incrementing index, provide a command
-# that could be run to add this
-
-# check which spearator to use, dash might be confusing
-
-# @cmd_setup.command('test')
-# def test_code():
-#     """Test a `code` instance for a Quantum ESPRESSO executable."""
-#     from ase.build import bulk
-
-#     # Create a silicon crystal structure
-#     structure = bulk('Si')
-#     structure = orm.StructureData(ase=structure)
-
-#     # Create a Quantum ESPRESSO calculation
-#     calculation = orm.CalcJobNode()
+        echo.echo_report(f'Submitting PwBaseWorkChain with code<{code.label}>')
+        process = submit(builder)
+        echo.echo_report(f'PwBaseWorkChain<{process.pk}> submitted')
