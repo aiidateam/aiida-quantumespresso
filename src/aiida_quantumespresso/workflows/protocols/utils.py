ac56a72dda79ee31e11afcde2ac5b53d7b50181c
Edward Linscott <edwardlinscott@gmail.com>
2026-07-08 11:26:34 +0200

    Remove comment -- it's noise

--- src/aiida_quantumespresso/workflows/protocols/utils.py
+++ src/aiida_quantumespresso/workflows/protocols/utils.py
@@ -100,8 +100,6 @@ class ProtocolMixin:
             'core.slurm',
             'core.pbspro',
             'core.torque',
-            # HyperQueueJobResource accepts the num_machines/num_mpiprocs_per_machine
-            # backward-compatibility shape and derives num_cpus from it.
             'hyperqueue',
         ):
             new_options.setdefault('resources', {}).setdefault('num_machines', 1)
